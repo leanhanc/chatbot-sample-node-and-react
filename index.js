defffb81f897c212bb22d0f5bdc587d74a82ee88
Leandro Hanc <leandrohanc@gmail.com>
2019-07-09 18:18:50 -0300

    Add dialog flow routes

--- index.js
+++ index.js
@@ -4,9 +4,15 @@ const app = express()
 
 const PORT = process.env.PORT || 6727
 
+// MIDDLEWARE
 app.use(bodyParser.json())
 
-app.get("/", (req, res) => {
+// ROUTES
+app.get("/api/df_text_query", (req, res) => {
+  console.log("test")
+})
+
+app.get("/api/df_event_query", (req, res) => {
   console.log("test")
 })
 
