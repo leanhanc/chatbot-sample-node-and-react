194dee9598fb00dfa0292d97fd7c6574065bd142
Leandro Hanc <leandrohanc@gmail.com>
2019-07-09 18:17:31 -0300

    Add body-parser

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express")
+const bodyParser = require("body-parser")
 const app = express()
 
 const PORT = process.env.PORT || 6727
+
+app.use(bodyParser.json())
 
 app.get("/", (req, res) => {
   console.log("test")
