41bc0fb37707542834493720ff6cec3a31758df3
Leandro Hanc <leandrohanc@gmail.com>
2019-07-13 11:35:13 -0300

    Add front-end

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require("express")
 const bodyParser = require("body-parser")
 const app = express()
@@ -10,7 +11,13 @@ app.use(bodyParser.json())
 // MOUNT ROUTES
 require("./routes/dialogFlowRoutes")(app)
 
-
+// MOUNT FRONT-END
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 // START APP
 app.listen(PORT)
